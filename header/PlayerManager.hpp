7b8c34a28699096beeeeaf96f73d56ec20c3925f
emii331 <emilyclark5293@gmail.com>
2023-05-26 07:38:39 -0700

    Updated StatsManager

--- header/PlayerManager.hpp
+++ header/PlayerManager.hpp
@@ -1,12 +1,31 @@
 #ifndef STATSMANAGER_H
 #define PLAYERMANAGER_H
-#include <string>
-#include <iostream>
  
 using namespace std;
 
 class PlayerManager {
+    private:
+        int playerType;
+
+        int reputation = 0;
+        int morality = 0;
+        int popularReputation = 0;
+        int normieReputation = 0;
+        int outcastReputation = 0;
     
+    public:
+        void setPlayerType(int);
+
+        void setPlayerRep(int);
+        int getPlayerRep();
+        void setPlayerMor(int);
+        int getPlayerMor(); 
+        void setPopularRep(int);
+        int getPopularRep();      
+        void setNormieRep(int);
+        int getNormieRep();
+        void setOutcastRep(int);
+        int getOutcastRep();
 };
 
 
--- src/StatsManager.cpp
+++ src/StatsManager.cpp
@@ -1,7 +1,66 @@
 #include "../header/StatsManager.hpp"
 
-bool StatsManager::updateStats(PlayerManager& player, int result, string npcType) {
-    int multiplier = 1;
+void StatsManager::updateStats(PlayerManager& player, int reputationResult, int moralityResult, string npcType) {
+    int successMultiplier = 1;
+    int failMultiplier = 2;
 
-    if (npcType)
+    // Update multiplier based on npc/character type
+    // TO FIX: player type values based on those used in playerManager class
+    if (npcType == "Popular" || npcType == "Alpha Male") {
+        if (player.getPlayerType() == 0) {
+            successMultiplier = 2;
+            failMultiplier = 1;
+        }
+    }
+    else if (npcType == "Normie" && player.getPlayerType() == 1) {
+        multiplier = 2;
+        failMultiplier = 1;
+    }
+    else if (npcType == "Outcast" && player.getPlayerType() == 2) {
+        multiplier = 2;
+        failMultiplier = 1;
+    }
+
+    // Update player reputation based on results of npc interaction
+    // TO FIX: reputationResult value based on those used in Interaction class
+    if (reputationResult == 0) {
+        player.setPlayerRep(player.getPlayerRep() - ( 1 * multiplier) ) ;
+    }
+    else {
+        player.setPlayerRep(player.getPlayerRep() + ( 1 * multiplier));
+    }
+
+    if (npcType == "Popular" || npcType == "Alpha Male") {
+        if (reputationResult == 0) {
+            player.setPopularRep(player.getPopularRep() - ( 1 * multiplier) ) ;
+        }
+        else {
+            player.setPopularRep(player.getPopularRep() + ( 1 * multiplier));
+        }
+    }
+    else if (npcType == "Normie") {
+         if (reputationResult == 0) {
+            player.setNormieRep(player.getNormieRep() - ( 1 * multiplier) ) ;
+        }
+        else {
+            player.setNormieRep(player.getNormieRep() + ( 1 * multiplier));
+        }       
+    }
+    else if (npcType == "Outcast") {
+         if (reputationResult == 0) {
+            player.setOutcastRep(player.getOutcastRep() - ( 1 * multiplier) ) ;
+        }
+        else {
+            player.setOutcastRep(player.getOucastRep() + ( 1 * multiplier));
+        }       
+    }
+
+    // Update player morality based on result of npc interaction
+    if (moralityResult == 0) {
+        player.setPlayerMor(player.getPlayerMor() - 1) ;
+    }
+    else {
+        player.setPlayerMor(player.getPlayerMor() + 1);
+    }
+
 }
