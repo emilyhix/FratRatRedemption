405be24b573080e0609904a966bb8bacf5e41266
bphimmala <bphimmala@gmail.com>
2023-06-04 17:53:28 -0700

    Added characterCustomization feature

---
+++ src/PlayerManager.cpp
@@ -0,0 +1,27 @@
+#include "../header/PlayerManager.hpp"
+#include <iostream>
+#include <string>
+
+void playerManager::setPlayerName(){
+    cin >> playerName;
+}
+
+void playerManager::setPlayerType(const int &chosenType){
+    if(chosenType == 1){
+        playerType = "Popular";
+    }
+    else if(chosenType == 2){
+        playerType = "Normie";
+    }
+    else if(chosenType == 3){
+        playerType == "Outcast";
+    }
+}
+
+string playerManager::getPlayerName(){
+    return playerName;
+} 
+
+string playerManager::getPlayerType(){
+    return playerType;
+}
