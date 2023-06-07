aa63f928a1f16aa82a7d6cbfe4a6e1b9bd9dc3a2
emii331 <emilyclark5293@gmail.com>
2023-06-07 15:48:15 -0700

    Updated endingTest file

--- test/endingTest.cpp
+++ test/endingTest.cpp
@@ -4,13 +4,13 @@
 
 
 TEST(EndingManagerTests, lowMoralityEnding) {
-    EndingManager *end = new EndingManager(10,5,5,4,3,"popular");
+    EndingManager *end = new EndingManager(10,5,5,4,3,"Popular");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 
 TEST(EndingManagerTests, lowReputationEnding) {
-    EndingManager *end = new EndingManager(10,10,-2,-3,-4,"popular");
+    EndingManager *end = new EndingManager(10,10,-2,-3,-4,"Popular");
     EXPECT_NO_THROW(end->printEnding());
 }
 
@@ -19,50 +19,50 @@ TEST(EndingManagerTests, lowReputationEnding) {
 
 
 TEST(EndingManagerTests, popular_popular) {
-    EndingManager *end = new EndingManager(10,10,5,4,3,"popular");
+    EndingManager *end = new EndingManager(10,10,5,4,3,"Popular");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 TEST(EndingManagerTests, popular_normie) {
-    EndingManager *end = new EndingManager(10,10,4,5,3,"popular");
+    EndingManager *end = new EndingManager(10,10,4,5,3,"Popular");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 TEST(EndingManagerTests, popular_outcast) {
-    EndingManager *end = new EndingManager(10,10,4,3,5,"popular");
+    EndingManager *end = new EndingManager(10,10,4,3,5,"Popular");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 
 
 TEST(EndingManagerTests, normie_popular) {
-    EndingManager *end = new EndingManager(10,10,5,4,3,"normie");
+    EndingManager *end = new EndingManager(10,10,5,4,3,"Normie");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 TEST(EndingManagerTests, normie_normie) {
-    EndingManager *end = new EndingManager(10,10,4,5,3,"normie");
+    EndingManager *end = new EndingManager(10,10,4,5,3,"Normie");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 TEST(EndingManagerTests, normie_outcast) {
-    EndingManager *end = new EndingManager(10,10,4,3,5,"normie");
+    EndingManager *end = new EndingManager(10,10,4,3,5,"Normie");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 
 
 TEST(EndingManagerTests, outcast_popular) {
-    EndingManager *end = new EndingManager(10,10,5,4,3,"outcast");
+    EndingManager *end = new EndingManager(10,10,5,4,3,"Outcast");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 TEST(EndingManagerTests, outcast_normie) {
-    EndingManager *end = new EndingManager(10,10,4,5,3,"outcast");
+    EndingManager *end = new EndingManager(10,10,4,5,3,"Outcast");
     EXPECT_NO_THROW(end->printEnding());
 }
 
 TEST(EndingManagerTests, outcast_outcast) {
-    EndingManager *end = new EndingManager(10,10,4,3,5,"outcast");
+    EndingManager *end = new EndingManager(10,10,4,3,5,"Outcast");
     EXPECT_NO_THROW(end->printEnding());
 }
