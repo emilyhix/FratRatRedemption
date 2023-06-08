5f816bc258fd17a7bab0416103e03ceec45df71f
gmapa002 <gmapa002@ucr.edu>
2023-06-08 01:04:37 -0700

    Setup for playerActor tests

---
+++ test/playerActorTest.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "../header/playerActor.hpp"
+#include "../src/playerActor.cpp"
+
+TEST(playerActorTests, runningConstructor) {
+    EXPECT_NO_THROW(playerActor *testActor = new playerActor());
+}
+
+TEST(playerActorTests, constructorInitialValues) {
+    playerActor *testActor = new playerActor();
+
+    EXPECT_EQ(testActor->getXPos(),61);
+    EXPECT_EQ(testActor->getYPos(),6);
+}
+
+// TEST(coordTests, constructorInitialValues) {
+//     coord *testCoord = new coord(); 
+
+//     EXPECT_EQ(testCoord->getCoordCharacter(),' ');
+//     EXPECT_EQ(testCoord->getCoordColor(),"\033[37m");
+//     EXPECT_EQ(testCoord->getWalkable(),1);
+//     EXPECT_EQ(testCoord->getPlayerActive(),0);
+//     EXPECT_EQ(testCoord->getContainsNPC(),0);
+// }
+
+// TEST(coordTests, colorFunctions) {
+//     coord *testCoord = new coord(); 
+
+//     EXPECT_NO_THROW(testCoord->setCoordColor("poop"));
+//     EXPECT_EQ(testCoord->getCoordColor(),"poop"); 
+// }
+
+// TEST(coordTests, characterFunctions) {
+//     coord *testCoord = new coord(); 
+
+//     EXPECT_NO_THROW(testCoord->setCoordCharacter('#'));
+//     EXPECT_EQ(testCoord->getCoordCharacter(),'#');  
+// }
+
+// TEST(coordTests, walkableFunctions) {
+//     coord *testCoord = new coord(); 
+
+//     EXPECT_NO_THROW(testCoord->toggleWalkable());
+//     EXPECT_EQ(testCoord->getWalkable(),0);  
+// }
+
+// TEST(coordTests, playerFunctions) {
+//     coord *testCoord = new coord(); 
+
+//     EXPECT_NO_THROW(testCoord->togglePlayerActive());
+//     EXPECT_EQ(testCoord->getPlayerActive(),1); 
+// }
+
+// TEST(coordTests, npcFunctions) {
+//     coord *testCoord = new coord(); 
+
+//     EXPECT_NO_THROW(testCoord->toggleContainsNPC());
+//     EXPECT_EQ(testCoord->getContainsNPC(),1); 
+// }
