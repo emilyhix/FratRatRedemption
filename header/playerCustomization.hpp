ff8abc858b5c3bb984d7b1084f30e791c332a4ce
bphimmala <bphimmala@gmail.com>
2023-06-07 22:59:28 -0700

    Reworked playerCustom class to not violate SRP

--- header/playerCustomization.hpp
+++ header/playerCustomization.hpp
@@ -7,11 +7,18 @@ using namespace std;
 class playerCustom{
     private:
         string customizationLines[5];
+        string playerName;
+        string playerType;
     
     public:
         playerCustom();
+        void setPlayerName();
+        string getPlayerName();
+        void setPlayerType();
+        string getPlayerType();
+        void printCustomizationLine(int);
         void createPlayer(PlayerManager &);
-
+        
 
 };
 
--- src/playerCustomization.cpp
+++ src/playerCustomization.cpp
@@ -13,18 +13,61 @@ playerCustom::playerCustom(){
 
 }
 
+void playerCustom::setPlayerName(){
+    cin >> playerName;
+}
+
+string playerCustom::getPlayerName(){
+    return playerName;
+}
+
+void playerCustom::setPlayerType(){
+    bool validType = 0;
+    int chosenNum = 0;
+
+    while(validType == false){
+        cout << "1. Popular" << endl << "2. Normie" << endl << "3. Outcast" << endl << endl;
+        cin >> chosenNum;
+
+        if(chosenNum == 1){
+            playerType = "Popular";
+            validType = true;
+        }
+        else if(chosenNum == 2){
+            playerType = "Normie";
+            validType = true;
+        }
+        else if(chosenNum == 3){
+            playerType = "Outcast";
+            validType = true;
+        }
+        else{
+            cout << "INVALID TYPE SELECTED." << endl << endl;
+            validType = false;
+        }
+    }
+}
+
+string playerCustom::getPlayerType(){
+    return playerType;
+}
+
+void playerCustom::printCustomizationLine(int num){
+    cout << customizationLines[num] << endl;
+}
+
 void playerCustom::createPlayer(PlayerManager &player){
-    cout << customizationLines[0] << endl;
-    cout << customizationLines[1] << endl << endl;
+    printCustomizationLine(0);
+    printCustomizationLine(1);
 
     player.setPlayerName();
     cout << endl;
 
-    cout << player.getPlayerName() << ". " << customizationLines[2] << endl;
-    cout << "1. Popular" << endl << "2. Normie" << endl << "3. Outcast" << endl << endl;
+    cout << player.getPlayerName() << ". ";
+    printCustomizationLine(2);
+    cout << endl;
    
     player.setPlayerType();
-
 
     if (player.getPlayerType() == "Outcast"){
         cout << "Oh, so you're an " << player.getPlayerType() << "?" << endl << endl;
@@ -32,8 +75,9 @@ void playerCustom::createPlayer(PlayerManager &player){
     else{
         cout << "Oh, so you're a " << player.getPlayerType() << "?" << endl << endl;
     }
-    cout << customizationLines[3] << endl;
-    cout << customizationLines[4] << endl;
+
+    printCustomizationLine(3);
+    printCustomizationLine(4);
 
     bool ready_flag = false;
     while(ready_flag == false){
@@ -50,4 +94,3 @@ void playerCustom::createPlayer(PlayerManager &player){
         }
     }
 }
-
